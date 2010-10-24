233cb9a7c50b523c42e9817b1fa4e44549c3dde7
Matt Nunes <endoplasmicable@gmail.com>
2010-10-23 20:48:08 -0400

    start over!

--- app/models/course.rb
+++
@@ -1,4 +0,0 @@
-class Course < ActiveRecord::Base
-  include ClassList
-  has_and_belongs_to_many :schedules
-end
--- app/models/schedule.rb
+++
@@ -1,8 +0,0 @@
-class Schedule < ActiveRecord::Base
-    belongs_to :user
-    has_and_belongs_to_many :courses
-    
-    def include?(x)
-      courses.include?(x)
-    end
-end
