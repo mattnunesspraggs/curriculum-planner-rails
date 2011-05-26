b2fe7478101b37b48b093c6ad26a1d90ca0e4222
Matthew Nunes <matt@mattnunes.com>
2011-05-26 01:52:49 -0400

    date change... again.
    Signed-off-by: Matthew Nunes <matt@mattnunes.com>

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,8 +2,8 @@ CURR_DAYS = {1 => "M", 2 => "T", 3 => "W", 4 => "Th", 5 => "F", 6 => "Sa", 0 => 
 DAYS = {0 => "SU", 1 => "MO", 2 => "TU", 3 => "WE", 4 => "TH", 5=> "FR", 6 => "SA" }
 FULL_DAYS = {0 => "Sunday", 1 => "Monday", 2 => "Tuesday", 3 => "Wednesday", 4 => "Thursday", 5=> "Friday", 6 => "Saturday" }
 
-CLASSES_START = Date.new(2011, 08, 31)
-CLASSES_END = Date.new(2011, 12, 08)
+CLASSES_START = Date.new(2011, 8, 31)
+CLASSES_END = Date.new(2011, 12, 8)
 
 SUBJECT_TAGS = YAML::load(File.read("config/tags.yaml"))
 
